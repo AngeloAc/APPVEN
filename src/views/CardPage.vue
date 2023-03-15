398063635ba7e7190aff56d470cd4fdaf9f45a1e
AngeloAc <euclidescarvalho1904@gmail.com>
2023-03-15 12:34:15 +0100

    updated

---
+++ src/views/CardPage.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>This page show us SHOPPING CART PAGE</h1>
+</template>
+
+<script>
+    export default{
+        name: 'CardPage',
+    };
+</script>
+
+<style scoped>
+h1{
+    color: green;
+}
+</style>
---
+++ src/views/ProductPage.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>This page show us PRODUCT PAGE</h1>
+</template>
+
+<script>
+    export default{
+        name: 'ProductPage',
+    };
+</script>
+
+<style scoped>
+h1{
+    color: red;
+}
+</style>
