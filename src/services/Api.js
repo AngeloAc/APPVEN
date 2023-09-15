97f3ca5d3a4e6e0bbfd39a8edb4154699b26ef61
AngeloAc <euclidescarvalho1904@gmail.com>
2023-09-15 13:56:32 +0100

    local ip

--- src/services/Api.js
+++ src/services/Api.js
@@ -4,6 +4,6 @@ import axios from 'axios';
 export default () => axios.create(
     {
         
-        baseURL: "http://127.0.0.1:3030/user"
+        baseURL: "http://localhost:3030/user"
     }
 );
